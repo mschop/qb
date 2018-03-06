0b164b561f78202f634682ccb0cac93a6be678d8
Martin Schophaus <martin.schophaus@jtl-software.com>
2018-03-06 13:50:49 +0100

    Fix select expr for use with alias.* selection

--- src/Fragments/SelectFragment.php
+++ src/Fragments/SelectFragment.php
@@ -14,20 +14,41 @@ class SelectFragment extends QueryBuilder
 
     public function __construct(QueryBuilder $prev, $select, string $alias = null)
     {
-        if(!$select instanceof Expression) {
-            Security::validateIdentifier($select);
+        if($select instanceof Expression) {
+            $this->select = $select;
+        } else {
+            $dotExploded = explode('.', $select);
+            $dotExploded = array_map('trim', $dotExploded);
+            foreach($dotExploded as $part) {
+                if($part !== '*') {
+                    Security::validateIdentifier($select);
+                }
+            }
+            $this->select = $dotExploded;
         }
+
         if($alias !== null) {
             Security::validateIdentifier($alias);
         }
         parent::__construct($prev);
-        $this->select = $select;
         $this->alias = $alias;
     }
 
     public function __toString()
     {
-        $select = $this->select instanceof Expression ? $this->select : "`{$this->select}`";
+        if ($this->select instanceof Expression) {
+            $select = $this->select;
+        } else {
+            $parts = [];
+            foreach($this->select as $part) {
+                if($part === '*') {
+                    $parts[] = '*';
+                } else {
+                    $parts[] = '`' . $part . '`';
+                }
+            }
+            $select = implode('.', $parts);
+        }
         $alias = $this->alias !== null ? " AS `{$this->alias}`" : "";
         return $select . $alias;
     }
@@ -47,4 +68,5 @@ class SelectFragment extends QueryBuilder
     {
         return [];
     }
+
 }
